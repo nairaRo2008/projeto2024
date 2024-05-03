8c925a0a86f85b85856b58e0fbee0a7a33047c82
nairaRo2008 <143710365+nairaRo2008@users.noreply.github.com>
2024-05-03 07:13:36 -0300

    Update funcoes_javascript.js

--- funcoes_javascript.js
+++ funcoes_javascript.js
@@ -5,5 +5,8 @@ function multiplicar (num1, num2) {
   return num1xnum2;
 }
 function diminuir (num1, num2) {
-  return num1-num2:
+  return num1-num2;
+}
+function dividir (numer4, num2) {
+  return num4/num2.
 }
